5495e31b5be056619d3087f4ba0f260ebf335c66
Takahiro Iha <e165719@ie.u-ryukyu.ac.jp>
2018-10-25 15:12:45 +0900

    GPAとLabのバリデーションの追加

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
 
   VALID_IE_NUMBER_REGEX = /\A[0-9]{2}57+[0-9]{2}\z/
   validates :ie_number, presence: true,
-                        length: { maximum: 50 },
                         format: { with: VALID_IE_NUMBER_REGEX }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,4 +13,11 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,
                        length: { minimum: 6 }
+
+  VALID_GPA_REGEX = /\A[0-3]+\.+[0-9]{2}\z|\A[4]+\.+[0]{2}\z/
+  validates :gpa, presence: true,
+                  format: { with: VALID_GPA_REGEX }
+
+  validates :lab, presence: true
+                  
 end
