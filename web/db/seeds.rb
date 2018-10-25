3601a92eaba357b89964b6f43fa5ccd5ddc4b06f
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-25 17:46:15 +0900

    testユーザの作成

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+lab = Array.new()
+15.times do
+  lab << Faker::Japanese::Name.last_name # https://github.com/tily/ruby-faker-japanese
+end
+
+60.times do |n|
+  email = Faker::Internet.email
+  gpa = format("%.2f",rand(0.0..3.9)).to_f
+  password = "password"
+  User.create!(id: n+1,
+               ie_number: "0057#{sprintf("%02d",n+1)}",
+               email: email,
+               gpa: gpa,
+               lab: lab.sample,
+               password: password,
+               password_confirmation: password,
+               )
+end
